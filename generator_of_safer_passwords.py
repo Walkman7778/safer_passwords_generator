43d28da85153f84ed5ab464421ee9f9bed6667cf
PiotrTRN <108199441+Walkman7778@users.noreply.github.com>
2024-03-09 21:18:26 +0200

    Update generator_of_safer_passwords.py

--- generator_of_safer_passwords.py
+++ generator_of_safer_passwords.py
@@ -1,3 +1,4 @@
+# heading of the programm
 import random
 
 digits = '0123456789'
@@ -6,6 +7,7 @@ uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
 
 chars = ''
+# reading user data
 n = int(input('Introdusing numers of new passwords: '))
 length = int(input('Introduce length of password: '))
 add_digit = input('Introduce digits? (y = yes, n = not) ').strip()
@@ -14,7 +16,7 @@ add_uppercase = input('Introduce lowercase? (y = yes, n = not) ').strip()
 add_punctuation = input('Introducing such simbols as: !#$%&*+-=?@^_? (д = да, н = нет) ').strip()
 remove_badsymbols = input('Excluding casefold? (y = yes, n = not)').strip()
 
-
+# settings of generated passwords
 if add_digit.lower() == 'д':
     chars += digits
 if add_lowercase.lower() == 'д':
@@ -26,11 +28,12 @@ if add_punctuation.lower() == 'д':
 if remove_badsymbols.lower() == 'д':
     for c in 'il1Lo0O':
         chars = chars.replace(c, '')
+# function of generation of password
 def generate_password(length, chars):
     password = ''
     for j in range(length):
         password += random.choice(chars)
     return password
-
+# generation of required numers of passwrods
 for _ in range(n):
     print(generate_password(length, chars))
